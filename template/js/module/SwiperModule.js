d386fff46dd4aaea0cce303b608b4c94f77faab7
NHATDUY8996 <86871710+NHATDUY8996@users.noreply.github.com>
2023-08-02 14:49:16 +0700

    add slider

--- template/js/module/SwiperModule.js
+++ template/js/module/SwiperModule.js
@@ -3,12 +3,33 @@ export default function SwiperModule() {
     direction: "vertical",
     loop: true,
     autoplay: {
-      delay: 1000, // Thời gian chờ giữa các slide (tính bằng mili giây)
+      delay: 3000, // Thời gian chờ giữa các slide (tính bằng mili giây)
       disableOnInteraction: false, // Cho phép autoplay tiếp tục sau khi người dùng tương tác với swiper
     },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
-    },
+    }
+  });
+  $(document).ready(function () {
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: true,
+      navText: ["<i class='fa-solid fa-arrow-left'></i>",
+        "<i class='fa-solid fa-arrow-right'></i>"], // Thay đổi icon nút điều hướng
+      items: 1,
+      dots: true,
+      onInitialized: function () {
+        var pagination = $('.owl-dots');
+        pagination.find('.owl-dot').each(function (index) {
+          var btnLastPost = $('.pagination .lastPost-click').eq(index); // Lấy button tương ứng
+          btnLastPost.on('click', function () {
+            owl.trigger('to.owl.carousel', [index, 300]); // Di chuyển đến phần tử tương ứng
+          });
+        });
+      },
+    });
   });
 }
