a6b4f99306be549cfdcf2ea275ca8d6b8c7518b5
NHATDUY8996 <86871710+NHATDUY8996@users.noreply.github.com>
2023-08-02 18:06:04 +0700

    add

--- template/js/module/SwiperModule.js
+++ template/js/module/SwiperModule.js
@@ -1,4 +1,5 @@
 export default function SwiperModule() {
+  // slider banner
   var bannerswiper = new Swiper(".bannerSwiper", {
     direction: "vertical",
     loop: true,
@@ -11,14 +12,17 @@ export default function SwiperModule() {
       clickable: true,
     }
   });
+  // slider last post
   $(document).ready(function () {
-    var owl = $('.owl-carousel');
+    var owl = $('.lastPost-carousel');
     owl.owlCarousel({
-      loop: true,
+      loop: false,
       margin: 10,
       nav: true,
-      navText: ["<i class='fa-solid fa-arrow-left'></i>",
-        "<i class='fa-solid fa-arrow-right'></i>"], // Thay đổi icon nút điều hướng
+      navText: [
+        "<i class='fa-solid fa-arrow-left'></i>",
+        "<i class='fa-solid fa-arrow-right'></i>",
+      ], // Thay đổi icon nút điều hướng
       items: 1,
       dots: true,
       onInitialized: function () {
@@ -26,10 +30,35 @@ export default function SwiperModule() {
         pagination.find('.owl-dot').each(function (index) {
           var btnLastPost = $('.pagination .lastPost-click').eq(index); // Lấy button tương ứng
           btnLastPost.on('click', function () {
-            owl.trigger('to.owl.carousel', [index, 300]); // Di chuyển đến phần tử tương ứng
+            // Loại bỏ class active khỏi các button
+            $('.lastPost-click').removeClass('active');
+
+            // Thêm class active vào button được click
+            $(this).addClass('active');
+
+            // Di chuyển đến phần tử tương ứng
+            owl.trigger('to.owl.carousel', [index, 300]);
           });
         });
       },
+      onChanged: function (event) {
+        // Loại bỏ class active khỏi tất cả các button
+        $('.lastPost-click').removeClass('active');
+
+        // Thêm class active vào button tương ứng với slide hiện tại
+        var currentSlideIndex = event.item.index;
+        $('.lastPost-click').eq(currentSlideIndex).addClass('active');
+      },
     });
+  });
+
+  // slider ai update
+  var aiUpdateSwiper = new Swiper(".aiupdateSwiper", {
+    loop: true,
+    loopAdditionalSlides: 1, //để thêm 2 slide vào cả hai bên của swiper
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
   });
 }
